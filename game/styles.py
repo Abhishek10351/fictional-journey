533e79c31ca95743b15f8ec16d39338a00bee9cf
Abhishek Kashyap <abhik5567@gmail.com>
2022-08-14 19:53:06 +0530

    add styles for gui

---
+++ game/styles.py
@@ -0,0 +1,12 @@
+import arcade
+danger_style = {
+    "font_name": ("calibri", "arial"),
+    "font_size": 15,
+    "font_color": arcade.color.WHITE,
+    "border_width": 2,
+    "border_color": None,
+    "bg_color": arcade.color.REDWOOD,
+    "bg_color_pressed": arcade.color.WHITE,
+    "border_color_pressed": arcade.color.RED,
+    "font_color_pressed": arcade.color.RED,
+}

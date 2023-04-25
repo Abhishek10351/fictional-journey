7c52e01d62ae9efbcea56fa1a195b6e080540604
Abhishek Kashyap <abhik5567@gmail.com>
2023-04-26 00:04:54 +0530

    add a settings page

--- game/views/settings.py
+++ game/views/settings.py
@@ -1,5 +1,22 @@
 import arcade
 import arcade.gui
 
+
 class SettingsView(arcade.View):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.settings_screen = arcade.gui.widgets.layout.UIBoxLayout()
+        
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+        # self
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
+    
+    def on_show_view(self):
+        self.manager.enable()
+    
+    def on_hide_view(self):
+        self.manager.disable()

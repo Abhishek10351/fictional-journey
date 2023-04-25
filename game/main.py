44c81a9bae8c163f119933f59c5afc32ee699872
Abhishek Kashyap <abhik5567@gmail.com>
2023-04-25 23:52:49 +0530

    change game icon

--- game/main.py
+++ game/main.py
@@ -11,11 +11,12 @@ import views
 import styles
 from window import Window
 import pyglet.image
+from pathlib import Path
 
 if __name__ == "__main__":
     window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.set_icon(pyglet.image.load(
-        'assets/images/players/player_green.png'))
+        Path('assets/images/aliens/enemy.png')))
     window.views = {"Menu": views.Menu(), "LevelUp": views.LevelUpView(
     ), "GameOver": views.GameOverView(), "HowToPlay": views.HowToPlay()}
     window.show_view(window.views["Menu"])

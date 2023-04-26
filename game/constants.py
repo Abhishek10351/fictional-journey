3fe1c37152c2bdbd1bcd8b6fbe37293b66ea0fe3
Abhishek Kashyap <abhik5567@gmail.com>
2023-04-26 22:59:52 +0530

    add path to constants

--- game/constants.py
+++ game/constants.py
@@ -1,6 +1,15 @@
 """
 All the constants needed for the game
 """
+
+from pathlib import Path
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Space Invader"
+
+ASSETS_PATH = Path("assets")
+GUI_PATH = ASSETS_PATH / "gui"
+IMAGES_PATH = ASSETS_PATH / "images"
+SOUNDS_PATH = ASSETS_PATH / "sounds"
+MUSIC_PATH = ASSETS_PATH / "music"

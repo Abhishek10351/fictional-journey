ea00500861436c731b8156c97e9a72da50b3f6b7
Abhishek Kashyap <abhik5567@gmail.com>
2023-04-28 20:34:39 +0530

    add basic control for settings

--- game/views/settings.py
+++ game/views/settings.py
@@ -1,21 +1,87 @@
 import arcade
 import arcade.gui
-
+from datetime import timedelta
+from arcade.gui.widgets.slider import UISlider
+import styles
+import arcade.gui.widgets
 
 class SettingsView(arcade.View):
     def __init__(self):
         super().__init__()
         self.settings_screen = arcade.gui.widgets.layout.UIBoxLayout()
+
+        back_button = arcade.gui.widgets.buttons.UIFlatButton(text="Back", width=200)
+        #texture_test_button = arcade.gui.widgets.buttons.UITextureButton(text="lol",texture=arcade.load_texture("assets/gui/buttons/blue_button08.png"), texture_hovered=arcade.load_texture("assets/gui/buttons/blue_button07.png"), texture_pressed=arcade.load_texture("assets/gui/buttons/blue_button09.png"),width=50, height=50)
+        a = arcade.TextureAtlas((50,50), textures=[arcade.load_texture("assets/gui/musicOn.png"),arcade.load_texture("assets/gui/button_textures/blue_button03.png")])
+
+        music_button = arcade.gui.widgets.buttons.UITextureButton(texture=arcade.load_texture("assets/gui/musicOn.png"), texture_hovered=arcade.load_texture(
+            "assets/gui/musicOn.png"), texture_pressed=arcade.load_texture("assets/gui/musicOff.png"), width=50, height=50, style=styles.dark_button)
+        # music_button.do_render(surface)
+        music_button.with_background(texture=arcade.load_texture(
+            "assets/gui/button_textures/blue_button03.png"))
+        sound_button = arcade.gui.widgets.buttons.UITextureButton(texture=arcade.load_texture("assets/gui/soundOn.png"), texture_hovered=arcade.load_texture("assets/gui/soundOn.png"), texture_pressed=arcade.load_texture("assets/gui/soundOff.png"), width=50, height=50)
+
+        self.sound_control = arcade.gui.widgets.layout.UIBoxLayout(vertical = False)
+
+
+        self.label = arcade.gui.widgets.text.UILabel(text="Settings", font_size=30)
+        slider = UISlider(value=50, width=300, height=50)
+
+        self.sound_control.add(music_button)
+        self.sound_control.add(sound_button)
+        self.settings_screen.add(back_button)
+        self.settings_screen.add(self.label)
+        # self.settings_screen.add(texture_test_button)
+        self.settings_screen.add(slider)
+
+
+        self.ui_anchor_layout = arcade.gui.widgets.layout.UIAnchorLayout()
+        self.ui_anchor_layout.add(self.sound_control, anchor_x="center_x", align_y=200)
+        self.ui_anchor_layout.add(self.settings_screen, anchor_x="center_x", anchor_y="center_y")
         
         self.manager = arcade.gui.UIManager()
+        self.manager.add(self.ui_anchor_layout)
         self.manager.enable()
 
-        # self
+        @back_button.event("on_click")
+        def on_back(event):
+            self.window.show_view(self.window.views["Menu"])
+        
+        @music_button.event("on_click")
+        def on_play_music(event):
+            self.window.play_music = not self.window.play_music
+            if self.window.play_music:
+                music_button.texture = arcade.load_texture("assets/gui/musicOn.png")
+                music_button.texture_hovered = arcade.load_texture("assets/gui/musicOn.png")
+                music_button.texture_pressed = arcade.load_texture("assets/gui/musicOff.png")
+            else:
+                music_button.texture = arcade.load_texture("assets/gui/musicOff.png")
+                music_button.texture_hovered = arcade.load_texture("assets/gui/musicOff.png")
+                music_button.texture_pressed = arcade.load_texture("assets/gui/musicOn.png")
+
+        @sound_button.event("on_click")
+        def on_play_sound(event):
+            self.window.play_sound = not self.window.play_sound
+            if self.window.play_sound:
+                sound_button.texture = arcade.load_texture("assets/gui/soundOn.png")
+                sound_button.texture_hovered = arcade.load_texture("assets/gui/soundOn.png")
+                sound_button.texture_pressed = arcade.load_texture("assets/gui/soundOff.png")
+            else:
+                sound_button.texture = arcade.load_texture("assets/gui/soundOff.png")
+                sound_button.texture_hovered = arcade.load_texture("assets/gui/soundOff.png")
+                sound_button.texture_pressed = arcade.load_texture("assets/gui/soundOn.png")
+
+        
+    def on_update(self, delta_time):
+        time = self.window.game_level_time
+        self.label.text = f"{time.seconds//60}:{time.seconds%60}:{time.microseconds//1000}"
+
     def on_draw(self):
         self.clear()
         self.manager.draw()
     
     def on_show_view(self):
+        self.window.game_level_time = timedelta(seconds=0)
         self.manager.enable()
     
     def on_hide_view(self):
--- game/window.py
+++ game/window.py
@@ -1,5 +1,6 @@
 import arcade
 import pyglet.media
+from datetime import timedelta
 
 
 class Window(arcade.Window):
@@ -8,15 +9,18 @@ class Window(arcade.Window):
         Set up the application.
         """
         super().__init__(width, height, title)
-        self.total_time = 0.0
-        self.sound = True
-        self.music = True
+        self.total_time = timedelta(seconds=0.0)
         self.total_levels = 2
         self.levels_completed = 0  # the last level completed
         self.current_level = 1  # the level currently played
         self.bg_music = pyglet.media.Player()
         self.bg_music.queue(pyglet.media.load("assets/music/funkyrobot.mp3"))
         self.completed = False
+        self.game_level_time = timedelta(seconds=0.0)
+        self.volume = 0.5
+        self.play_music = True
+        self.play_sound = True
 
     def on_update(self, delta_time):
-        self.total_time += delta_time
+        self.total_time += timedelta(seconds=delta_time)
+        self.game_level_time += timedelta(seconds=delta_time)

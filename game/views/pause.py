725cbaf40420f699a26f7b3fb6372cfe23e3f305
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-13 15:41:18 +0530

    add a back button in the pause screen

--- game/views/pause.py
+++ game/views/pause.py
@@ -8,11 +8,18 @@ class PauseScreen(arcade.View):
     def __init__(self):
         super().__init__()
 
+        back_button = BackButton()
+
+        @back_button.event
+        def on_click(event):
+            self.window.show_view(self.window.views["Menu"])
+
         message = "You have paused the game. \nYou can resume the game by pressing the ESC key. \n"
         self.text = arcade.gui.widgets.text.UITextArea(
             text=message, font_name="Kenney Future", width=450, height=200, font_size=20, bold=True, color=arcade.color.WHITE)
 
         self.manager = arcade.gui.UIManager()
+        self.manager.add(back_button)
         self.manager.add(arcade.gui.UIAnchorLayout(
             children=[self.text], anchor_x="center", anchor_y="center"))
 
@@ -28,7 +35,7 @@ class PauseScreen(arcade.View):
         self.clear()
         self.manager.draw()
 
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.ESCAPE:
+    def on_key_press(self, symbol, modifiers):
+        if symbol == arcade.key.ESCAPE:
             self.window.show_view(
                 self.window.levels[self.window.current_level - 1])

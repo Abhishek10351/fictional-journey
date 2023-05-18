94a03c12f12ef5032cc1a5150ac22c9293edd648
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-18 15:45:40 +0530

    rename button styles

--- game/styles/__init__.py
+++ game/styles/__init__.py
@@ -2,4 +2,4 @@ from .buttons import *
 from .texture_buttons import *
 
 __all__ = ["dark_button", "light_button", "primary_button", "danger_button", "success_button",
-           "dark_texture_button"]
+           "styled_texture_button"]

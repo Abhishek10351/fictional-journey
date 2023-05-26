4ff05e1b02e4cf6131ec4b82e05f78254f39ad4c
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-26 23:12:33 +0530

    move arrow icons

--- game/gui_widgets/backbutton.py
+++ game/gui_widgets/backbutton.py
@@ -6,14 +6,15 @@ from constants import SCREEN_HEIGHT
 
 
 class BackButton(arcade.gui.UITextureButton):
-    def __init__(self, **kwargs):
+    def __init__(self, back_view=None,**kwargs):
+        self.back_view = back_view
         super().__init__(
             texture=arcade.load_texture(
-                'assets/gui/arrowLeftBlack.png'),
+                'assets/gui/icons/arrowLeftBlack.png'),
             texture_hovered=arcade.load_texture(
-                'assets/gui/arrowLeftWhite.png'),
+                'assets/gui/icons/arrowLeftWhite.png'),
             texture_pressed=arcade.load_texture(
-                'assets/gui/arrowLeftBlack.png'),
+                'assets/gui/icons/arrowLeftBlack.png'),
             width=50, height=50, y=SCREEN_HEIGHT-50, **kwargs
         )
 

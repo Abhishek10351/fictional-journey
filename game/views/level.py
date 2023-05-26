609daf4b90c70d80211e929cb7958075927fe6d0
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-26 20:08:06 +0530

    add a label to show score(doesn't update)

--- game/views/level.py
+++ game/views/level.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 import pathlib
 from constants import *
 
+
 class Level(arcade.View):
     """ Base class for all levels """
 
@@ -11,17 +12,23 @@ class Level(arcade.View):
 
         self.total_time = timedelta(minutes=0, seconds=0, microseconds=0)
         self.background = arcade.load_texture(pathlib.Path(
-            "assets/images/background.jpg", width=3000, height=2000))
+            "assets/images/background.jpg"))
         self.score = 0
-        self.player=None
+        self.player = None
         self.bullets = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
         self.enemy_bullets = arcade.SpriteList()
+        self.manager = arcade.gui.UIManager()
 
     def setup(self):
         self.clear()
-        self.total_time = timedelta(minutes=0, seconds=0, microseconds=0)
+        self.total_time = timedelta(seconds=0)
+        self.manager.clear()
         self.score = 0
+        self.score_label = arcade.gui.UILabel(
+            text=f"{self.score:0>5}", x=SCREEN_WIDTH-120, y=SCREEN_HEIGHT-80, font_name="Kenney Future", font_size=20)
+
+        self.manager.add(self.score_label)
         self.bullets.clear()
         self.enemy_bullets.clear()
 
@@ -42,17 +49,24 @@ class Level(arcade.View):
     def on_draw(self):
         """ Draw this view """
         self.clear()
-        arcade.draw_lrwh_rectangle_textured(0, 0,SCREEN_WIDTH,SCREEN_HEIGHT,
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT,
                                             self.background)
         self.player.draw()
         self.bullets.draw()
         self.enemy_list.draw()
         self.enemy_bullets.draw()
-    
+        self.manager.draw()
+
     def game_over(self):
         self.window.views["GameOver"].setup()
         self.window.show_view(self.window.views["GameOver"])
-    
+
     def level_complete(self):
         self.window.views["LevelUp"].setup()
         self.window.show_view(self.window.views["LevelUp"])
+
+    def on_show_view(self):
+        self.manager.enable()
+
+    def on_hide_view(self):
+        self.manager.disable()

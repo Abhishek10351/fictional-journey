ec244fa1d164fc6913fb69fd5e6ee72c3e480ba4
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-27 12:11:46 +0530

    update slider

--- game/gui_textures/slider.py
+++ game/gui_textures/slider.py
@@ -1,51 +1,57 @@
 import arcade
 from arcade.gui import UISlider
+from arcade.types import Color
 
 
 class UITextureSlider(UISlider):
-    """
-    A slider that supports textures
-    """
 
     def __init__(
         self,
-        thumb=arcade.load_texture("assets/gui/textures/thumb.png"),
         style=None,
         **kwargs
     ):
-        self.thumb = thumb
         style = style or UISlider.DEFAULT_STYLE
 
         super().__init__(style=style, **kwargs)
 
     def do_render(self, surface):
+        surface.clear()
         style: UISlider.UIStyle = self.get_current_style()
 
         self.prepare_render(surface)
-
-        # surface.draw_rectangle(0, 0, self.width, self.height, self.bar)
-        arcade.draw_rectangle_filled(
-            0, 0, self.width*2, self.height*2, arcade.color.WHITE)
 
         slider_height = self.height // 4
         slider_left_x = self._x_for_value(self.vmin)
         cursor_center_x = self.value_x
 
         slider_bottom = (self.height - slider_height) // 2
+        arcade.draw_xywh_rectangle_filled(
+            slider_left_x - self.x,
+            slider_bottom,
+            self.width-30,
+            slider_height-4,
+            Color(94, 104, 117, 255),
+        )
 
         arcade.draw_xywh_rectangle_filled(
             slider_left_x - self.x,
             slider_bottom,
             cursor_center_x - slider_left_x,
-            slider_height,
+            slider_height-4,
             style.filled_bar,
         )
 
         rel_cursor_x = cursor_center_x - self.x
+
+        if self.pressed:
+            thumb = arcade.load_texture(
+                "assets/gui/textures/thumb/dotWhite.png")
+        elif self.hovered:
+            thumb = arcade.load_texture(
+                "assets/gui/textures/thumb/dotGreen.png")
+        else:
+            thumb = arcade.load_texture(
+                "assets/gui/textures/thumb/dotBlue.png")
+
         surface.draw_texture(
-            x=rel_cursor_x - self.thumb.width // 4 + 2,
-            y=0,
-            width=self.thumb.width,
-            height=self.height,
-            tex=self.thumb,
-        )
+            rel_cursor_x - thumb.width // 2, thumb.width/2, thumb.width, thumb.width, thumb)

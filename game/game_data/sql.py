e22f2fe683896e8f7f3c3ae6794b8ba9554aa2cd
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-29 23:02:34 +0530

    use proper path for db

--- game/game_data/sql.py
+++ game/game_data/sql.py
@@ -2,7 +2,7 @@ import sqlite3
 from pathlib import Path
 
 
-db_path = Path(__file__) / "db.sqlite3"
+db_path = Path(__file__).parent / "db.sqlite3"
 connector = sqlite3.connect(db_path)
 cursor = connector.cursor()
 

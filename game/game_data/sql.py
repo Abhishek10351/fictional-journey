078e1c9ef963ed7ea2302557010d6b49bd2e0e2a
Abhishek Kashyap <abhik5567@gmail.com>
2023-05-29 20:10:24 +0530

    change db directory

--- game/game_data/sql.py
+++ game/game_data/sql.py
@@ -1,5 +1,5 @@
 import sqlite3
-connector = sqlite3.connect("db.sqlite3")
+connector = sqlite3.connect("game_data/db.sqlite3")
 cursor = connector.cursor()
 
 
@@ -17,15 +17,15 @@ default()
 
 
 def execute(query, *parameters):
-    cursor.execute(query, *parameters)
+    cursor.execute(query, parameters)
     connector.commit()
 
 
 def fetch(query, *parameters):
-    cursor.execute(query, *parameters)
+    cursor.execute(query, parameters)
     return cursor.fetchone()
 
 
 def fetchall(query, *parameters):
-    cursor.execute(query, *parameters)
+    cursor.execute(query, parameters)
     return cursor.fetchall()

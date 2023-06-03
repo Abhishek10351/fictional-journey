80ffee927641b07c69a458da5350495afe9e1699
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-03 20:25:40 +0530

    add powerups to db

--- game/game_data/sql.py
+++ game/game_data/sql.py
@@ -1,31 +1,45 @@
+import json
 import sqlite3
 from pathlib import Path
 
-
-db_path = Path(__file__).parent / "db.sqlite3"
+folder_path = Path(__file__).parent
+db_path = folder_path / "db.sqlite3"
+schema_path = folder_path / "tables.sql"
+powerups_path = folder_path / "powerups.json"
 connector = sqlite3.connect(db_path)
 cursor = connector.cursor()
 
 
 def default():
 
-    with open("game_data/tables.sql") as file:
+    with open(schema_path) as file:
         schemas = file.read()
         schemas = schemas.split("\n\n")
     for schema in schemas:
         cursor.execute(schema)
         connector.commit()
     add_settings()
+    add_powerups()
 
 
 def add_settings():
-    cursor.execute("SELECT * FROM settings;")
-    if not cursor.fetchone():
+    if not fetch("SELECT * FROM settings;"):
         cursor.execute("INSERT INTO settings VALUES (?, ?, ?);", (1, 1, 50))
         connector.commit()
 
 
-default()
+def add_powerups():
+    with open(powerups_path, 'r') as json_file:
+        powerups = json.load(json_file)["powerups"]
+
+    for powerup in powerups:
+        if not fetch("SELECT * FROM powerups WHERE id = ?", powerup["id"]):
+            cursor.execute('''INSERT INTO powerups
+                            VALUES (?, ?, ?, ?, ?, ?)''',
+                           (powerup["id"], powerup["name"], powerup["description"],
+                            powerup["duration"], powerup["rarity"], powerup["asset_path"]))
+
+    connector.commit()
 
 
 def execute(query, *parameters):
@@ -41,3 +55,6 @@ def fetch(query, *parameters):
 def fetchall(query, *parameters):
     cursor.execute(query, parameters)
     return cursor.fetchall()
+
+
+default()

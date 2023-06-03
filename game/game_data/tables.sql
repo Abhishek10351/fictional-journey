73336753d7232c42e2c8f416f87c618191893ac4
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-03 23:53:28 +0530

    add table schema

--- game/game_data/tables.sql
+++ game/game_data/tables.sql
@@ -13,7 +13,20 @@ CREATE TABLE IF NOT EXISTS settings (
   volume INTEGER NOT NULL CHECK (volume >= 0 AND volume <= 100)
 );
 
-CREATE TABLE IF NOT EXISTS high_scores (
+CREATE TABLE IF NOT EXISTS levels (
   level INTEGER PRIMARY KEY CHECK (level >= 1),
-  score INTEGER NOT NULL CHECK (score >= 0) DEFAULT 0
+  high_score INTEGER NOT NULL CHECK (high_score >= 0) DEFAULT 0,
+  description TEXT NOT NULL,
+  stars INTEGER NOT NULL CHECK (stars >= 0 AND stars <= 3) DEFAULT 0,
+  unlocked INTEGER NOT NULL CHECK (unlocked IN (0, 1)) DEFAULT 0,
+  completed INTEGER NOT NULL CHECK (completed IN (0, 1)) DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS powerups (
+    id VARCHAR(10) PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    duration INTEGER NOT NULL,
+    rarity TEXT NOT NULL,
+    path TEXT NOT NULL
 );

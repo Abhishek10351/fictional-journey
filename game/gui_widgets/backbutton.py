82daeafe66b1e31384c64dbc8cef6dc5377907f8
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-08 16:44:35 +0530

    re-design back button

--- game/gui_widgets/backbutton.py
+++ game/gui_widgets/backbutton.py
@@ -6,23 +6,13 @@ from constants import SCREEN_HEIGHT
 
 
 class BackButton(arcade.gui.UITextureButton):
-    def __init__(self, back_view=None,**kwargs):
-        self.back_view = back_view
+    def __init__(self, **kwargs):
         super().__init__(
             texture=arcade.load_texture(
-                'assets/gui/icons/arrowLeftBlack.png'),
+                'assets/gui/icons/arrow-left.png'),
             texture_hovered=arcade.load_texture(
-                'assets/gui/icons/arrowLeftWhite.png'),
+                'assets/gui/icons/arrow-left hover.png'),
             texture_pressed=arcade.load_texture(
-                'assets/gui/icons/arrowLeftBlack.png'),
-            width=50, height=50, y=SCREEN_HEIGHT-50, **kwargs
+                'assets/gui/icons/arrow-left click.png'),
+            width=50, height=50, y=SCREEN_HEIGHT-50, x=5,  **kwargs
         )
-
-    def on_update(self, dt):
-        if self.pressed:
-            color=arcade.color.WHITE
-        elif self.hovered:
-            color=arcade.color.BLACK
-        else:
-            color=arcade.color.WHITE
-        self.with_background(color=color)

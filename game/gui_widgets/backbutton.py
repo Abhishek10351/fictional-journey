c1c2aa36d32eee36f2eef042843551c8338ebd71
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-09 14:46:15 +0530

    improve backbutton

--- game/gui_widgets/backbutton.py
+++ game/gui_widgets/backbutton.py
@@ -2,17 +2,18 @@
 """
 import arcade
 import arcade.gui
-from constants import SCREEN_HEIGHT
+from constants import SCREEN_HEIGHT, GUI_PATH
 
 
 class BackButton(arcade.gui.UITextureButton):
     def __init__(self, **kwargs):
+        icons_path = GUI_PATH / "icons"
         super().__init__(
             texture=arcade.load_texture(
-                'assets/gui/icons/arrow-left.png'),
+                icons_path/'arrow-left.png'),
             texture_hovered=arcade.load_texture(
-                'assets/gui/icons/arrow-left hover.png'),
+                icons_path/'arrow-left hover.png'),
             texture_pressed=arcade.load_texture(
-                'assets/gui/icons/arrow-left click.png'),
+                icons_path/'arrow-left click.png'),
             width=50, height=50, y=SCREEN_HEIGHT-50, x=5,  **kwargs
         )

4291e11d7b172327dcb72761adc9dcd5c7fdee0f
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-10 23:37:09 +0530

    add a laser that shoots at an angle

--- game/sprites/__init__.py
+++ game/sprites/__init__.py
@@ -1,9 +1,9 @@
-from .players import Player
+from .players import *
 from .enemies import Enemy1
 from .enemy_lasers import EnemyLaser
 from .powerups import Powerup
-from .lasers import Laser
+from .lasers import *
 
 __all__ = [
-    "Player", "Laser", "EnemyLaser", "Enemy1", "Powerup"
+    "Player", "Laser", "Laser2", "EnemyLaser", "Enemy1", "Powerup"
 ]
--- game/sprites/lasers.py
+++ game/sprites/lasers.py
@@ -1,5 +1,6 @@
 from arcade import Sprite
 from constants import IMAGES_PATH
+import math
 
 
 class Laser(Sprite):
@@ -15,3 +16,16 @@ class Laser(Sprite):
     def update(self):
         self.center_y += 5
         self.top = max(0, self.top)
+
+
+class Laser2(Sprite):
+    """ Laser  that shoots at an angle"""
+
+    def __init__(self, image_path, **kwargs):
+        self.image_path = IMAGES_PATH / "lasers" / image_path
+        super().__init__(self.image_path, **kwargs)
+
+        angle = math.radians(self.angle)
+
+        self.change_x = math.sin(angle) * 5
+        self.change_y = math.cos(angle) * 5

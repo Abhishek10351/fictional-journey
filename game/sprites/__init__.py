7d3753d98444d5ca9859feab6c64747d17ff3bd9
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-11 14:12:44 +0530

    fix typo

--- game/sprites/__init__.py
+++ game/sprites/__init__.py
@@ -3,7 +3,6 @@ from .enemies import Enemy1
 from .enemy_lasers import EnemyLaser
 from .powerups import Powerup
 from .lasers import *
-
 __all__ = [
-    "Player", "Player2" "Laser", "Laser2", "EnemyLaser", "Enemy1", "Powerup"
+    "Player", "Player2", "Laser", "Laser2", "EnemyLaser", "Enemy1", "Powerup"
 ]

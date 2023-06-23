395a85b8189e365d992fd099a627093103851f25
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-23 16:12:15 +0530

    use TextureToggle instead of Toggle

--- game/gui_widgets/__init__.py
+++ game/gui_widgets/__init__.py
@@ -2,7 +2,7 @@ from .backbutton import BackButton
 from .levelbutton import LevelButton
 from .trophy_image import TrophyImage
 from .slider import UITextureSlider
-from .toggle import Toggle
+from .toggle import TextureToggle
 
 __all__ = [
     "BackButton", "LevelButton", "TrophyImage",

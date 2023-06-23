39e6e4962ef1c23ffd5200e28bb2f37893011c71
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-23 15:59:17 +0530

    add new file path to constants

--- game/constants.py
+++ game/constants.py
@@ -10,6 +10,8 @@ SCREEN_TITLE = "Space Invader"
 
 ASSETS_PATH = Path("assets")
 GUI_PATH = ASSETS_PATH / "gui"
+ICON_PATH = GUI_PATH / "icons"
+TEXTURES_PATH = GUI_PATH / "textures"
 IMAGES_PATH = ASSETS_PATH / "images"
 SOUNDS_PATH = ASSETS_PATH / "sounds"
 MUSIC_PATH = ASSETS_PATH / "music"

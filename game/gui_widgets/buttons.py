1061f5297a0e1ae3d64d9188b3eb5407ebafecc5
Abhishek Kashyap <abhik5567@gmail.com>
2023-06-25 00:11:37 +0530

    add a styles texture button

--- game/gui_widgets/buttons.py
+++ game/gui_widgets/buttons.py
@@ -27,3 +27,25 @@ class TextureButton(arcade.gui.UITextureButton):
             self._label.fit_content()
             self.ui_label.rect = self.ui_label.rect.max_size(
                 self.content_width, self.content_height)
+
+
+class StyledTextureButton(TextureButton):
+    def __init__(self, size="large", color="blue",
+                 oulined=False, flat=False, **kwargs):
+
+        texture_path = TEXTURES_PATH / "buttons" / size
+
+        texture = texture_path / f"{color}_button00.png"
+        texture_hover = texture_path / f"{color}_button01.png"
+        texture_pressed = texture_path / f"{color}_button02.png"
+        texture_disabled = texture_path / "grey_button00.png"
+
+        texture = arcade.load_texture(texture)
+        texture_hover = arcade.load_texture(texture_hover)
+        texture_pressed = arcade.load_texture(texture_pressed)
+        texture_disabled = arcade.load_texture(texture_disabled)
+
+        super().__init__(texture=texture, texture_hovered=texture_hover,
+                         texture_pressed=texture_pressed,
+                         texture_disabled=texture_disabled,
+                         style=styles.normal_button, **kwargs)

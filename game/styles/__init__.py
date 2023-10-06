d8e70ea28fc9981e4e139d795d173dc03084cfb3
Abhishek Kashyap <abhik5567@gmail.com>
2023-10-06 23:39:52 +0530

    add normal button

--- game/styles/__init__.py
+++ game/styles/__init__.py
@@ -1,4 +1,4 @@
 from .buttons import *
-
+from .texture_buttons import *
 __all__ = ["dark_button", "light_button", "primary_button", "danger_button",
-           "success_button"]
+           "success_button", "normal_button"]
